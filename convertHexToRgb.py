b05cd7a5b69a51e30ce507d859fff7d33620d219
ikmalsyafiq <56609721+ikmalsyafiq@users.noreply.github.com>
2021-10-06 18:44:32 -0700

    third attempt

--- convertHexToRgb.py
+++ convertHexToRgb.py
@@ -1,16 +1,22 @@
-def convert_hex_to_RGB(list):
+def convert_hex_to_RGB(list_of_hex):
     """convert hex to rgb
 
     Args:
-        list (list): a list of hex
+        list_of_hex (list): a list of hex
 
     Returns:
         list : a list of rgb tuples
-    """    
+    """ 
+    assert isinstance(list_of_hex,list)  
     rgb = []
-    for i in list:
+    for i in list_of_hex:
         h = i.lstrip('#')
         rgb.append(tuple(int(h[j:j+2], 16) for j in (0, 2, 4)))
     
     return rgb
+
+print(convert_hex_to_RGB(['##ffaBB','##ffaBB']))
+
+
+
 
