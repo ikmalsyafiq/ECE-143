07c8b87be1349268b0048171376a4aa739006f4d
ikmalsyafiq <56609721+ikmalsyafiq@users.noreply.github.com>
2021-11-12 08:45:27 -0800

    validation pass functional failed

---
+++ split_column.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from itertools import chain
+import collections
+
+def  split_count(x):
+    """split out column entries
+
+    Args:
+        x (dataframe): dataframe
+
+    Returns:
+        dataframe: dataframe with count
+    """    
+    #assert isinstance(x,pd)
+    splitted = []
+    for i in x['Is there anything in particular you want to use Python for?']:
+        splitted.append(i.split(','))
+        y = list(chain.from_iterable(splitted))
+    #print()
+    counter=collections.Counter(y)
+    counter
+    data = pd.DataFrame(counter.items())
+    data = data.rename(columns={0: '', 1: 'Count'})
+    data = data.sort_values(by = ['Count'],ascending=True)
+    data.reset_index(drop=True, inplace=True)
+    return data
+
+
+
+
